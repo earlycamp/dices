34cf24ce381d16e3ddcbd461941ba1474e10db32
Petermuturi <pmuturi65@gmail.com>
2017-07-04 13:37:40 +0300

    linked app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Content, Text } from 'native-base';
+
+export default ()=> {
+	return(
+		<Container>
+			<Content>
+				<Text>Hello</Text>
+			</Content>
+		</Container>
+		)
+}
